6cd9dcba64c83b2716237b11335e21eb5fb3b25f
maleenasultana <106397259+maleenasultana@users.noreply.github.com>
2023-08-12 00:47:25 +0530

    Update db.config.js

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     DB_NAME: " crm_db",
     DB_URL: "mongodb://localhost:27017/crm_db",
-    MONGO_URI:"mongodb+srv://maleena:masabtank@cluster0.6pah1dd.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_URI:"mongodb+srv://maleena:<password>@cluster0.6pah1dd.mongodb.net/?retryWrites=true&w=majority"
 }
 
